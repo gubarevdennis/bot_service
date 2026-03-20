6358e6c65f0e99ef7dad13e34683b4464ceb218b
Denis Gubarev <dengubareff@yandex.ru>
2026-03-20 15:54:16 +0300

    auth_service done

--- auth_service/main.py
+++ auth_service/main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from auth-service!")
+# auth_service/app/main.py
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from app.api.routes_auth import router as auth_router
+from app.db.session import engine
+from app.db.base import Base
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # При старте создаем таблицы
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
 
-if __name__ == "__main__":
-    main()
+app = FastAPI(title="Auth Service", lifespan=lifespan)
+
+# Подключаем роуты
+app.include_router(auth_router)
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
